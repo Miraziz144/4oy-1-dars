13c36fd43d4ed49f0ddfc45dd96a831aa90a4f06
Miraziz144 <nurullayevmiraziz4@gmail.com>
2024-06-26 08:32:46 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+let hafta = +prompt('son kiriting')
+
+switch (hafta) {
+    case 1:
+        alert('dushanba')
+        break;
+    case 2:
+        alert('seshanba')
+        break;
+    case 3:
+        alert('chorshanba')
+        break;
+    case 4:
+        alert('payshanba')
+        break
+    case 5:
+        alert('juma')
+        break
+    case 6:
+        alert('shanba')
+        break
+    case 7:
+        alert('yakshanba')
+        break
+    default:
+        alert('1 va 7 oraligida bolgan son kiriting')
+        break;
+}
+
+
+// let rang =+prompt('rang tanlang')
+
+// switch (rang) {
+//     case qizil:
+//         alert('toxta')
+//         break;
+//         case sariq:
+//             alert('tayyorlan')
+//     case yashil:
+//         alert('gazin bos')
+//         break
+//     default:
+//         break;
+// }
+
+
+// let radar = +prompt('tazlik kiritimg')
+
+// if (radar > 60) {
+//     alert('sekinro yur')
+// } else {
+//     alert('tezlash')
+// }
+
